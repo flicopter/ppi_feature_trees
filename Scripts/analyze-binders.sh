95f33761a0df2858f0ce34cf5a84097dc9d6ab82
flicopter <arrowcopter@gmail.com>
2014-10-28 21:46:09 +0900

    analyze-binders.sh added for pipelining analysis. config files are introduced.

--- Scripts/analyze-binders.sh
+++ Scripts/analyze-binders.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 ### usage
-# bash analyze-binders.R RECEPTOR_PROTEIN_DIR PARTNER_PDB
+# bash analyze-binders.sh CONFIG_FILE RECEPTOR_PROTEIN_DIR PARTNER_PDB
+source $1 
 
-PDBDIR=/home/yuri/Depot/PDB/benchmark4_structures/unbound/
-REFFILE=/home/yuri/Depot/rel/bench4_mono_bu_correct.tsv
-SCRIPTDIR=/home/yuri/src/ppi_feature_trees/Scripts/
 ANCHOR=$PWD
-RECNAME=$( echo $1 | sed 's/\///' )
-PDBID=$( echo $1 | sed 's/\///' ) 
-PARTNER_PDB=$2
+RECDIR=$2
+RECNAME=$( echo $2 | sed 's/\/$//' | awk -F '/' '{print $NF}')
+PARTNER_PDB=$( readlink -f $3 )
 
-cd $RECNAME/
+cd $RECDIR/
 rm -f *.pdb
 cp $PDBDIR/*.pdb .
-grep "^$PDBID" $REFFILE | awk '{print $2}' > binders.txt
+grep "^$RECNAME" $REFFILE | awk '{print $2}' > binders.txt
 for i in $RECNAME-*.out
   do python $SCRIPTDIR/contactmap.py $i $PARTNER_PDB
 done
-bash $SCRIPTDIR/movefile.sh $PDBID
+bash $SCRIPTDIR/movefile.sh $RECNAME
 
 Rscript $SCRIPTDIR/analyze-binders.R Binders No_Binders > classify_results.txt
 
