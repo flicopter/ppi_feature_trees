a013af6eabe0e869094e212a0ccb30021ad3db9b
jaak <jaak.simm@gmail.com>
2015-02-19 10:48:08 +0000

    analyze_binders.sh now checks that RECDIR/Binders and No_Binders exist

--- Scripts/analyze_binders.sh
+++ Scripts/analyze_binders.sh
@@ -8,6 +8,16 @@ fi
 SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 RECDIR="$1"
 
+if [ ! -d "$RECDIR/Binders" ]; then 
+    echo "The directory '$RECDIR/Binders' does not exist."
+    exit 1
+fi
+if [ ! -d "$RECDIR/No_Binders" ]; then
+    echo "The directory '$RECDIR/No_Binders' does not exist."
+    exit 1
+fi
+
 pushd $RECDIR
 Rscript $SCRIPTDIR/analyze_binders.R Binders No_Binders > classify_results.txt
+echo "Classification results written to '$RECDIR/classify_results.txt'."
 popd
